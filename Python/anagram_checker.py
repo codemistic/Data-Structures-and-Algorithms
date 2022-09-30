a9f67009127f51c940882b25b7dfc0392d3e751f
Arpit Rath <98519454+Ar-tech937@users.noreply.github.com>
2022-10-01 03:39:57 +0530

    Create anagram_checker.py

---
+++ Python/anagram_checker.py
@@ -0,0 +1,20 @@
+def anagram_checker(s1,s2):
+    list1 = list(s1)
+    list2 = list(s2)
+
+    list1.sort()
+    list2.sort()
+
+    pos = 0
+    matches = True
+
+    while pos < len(s1) and matches:
+        if list1[pos] == list2[pos]:
+            pos = pos + 1
+        else:
+            matches = False
+
+    return matches
+
+# Example - 
+# print(anagram_checker('abcde','edcba'))
