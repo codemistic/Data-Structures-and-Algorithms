037d6ba3defe4b68e76aa1c2c3801b5a57ac1cd7
sourav-naskar <souravnaskar156@gmail.com>
2022-10-02 00:55:27 +0530

    added dfs

---
+++ CPP/graph_tree/dfs.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+    void dfs(int node, vector<int> &vis, vector<int> adj[], vector<int> &storeDfs) {
+        storeDfs.push_back(node); 
+        vis[node] = 1; 
+        for(auto it : adj[node]) {
+            if(!vis[it]) {
+                dfs(it, vis, adj, storeDfs); 
+            }
+        }
+    }
+public:
+	vector<int>dfsOfGraph(int V, vector<int> adj[]){
+	    vector<int> storeDfs; 
+	    vector<int> vis(V+1, 0); 
+      for(int i = 1;i<=V;i++) {
+        if(!vis[i]) dfs(i, vis, adj, storeDfs); 
+      }
+	    return storeDfs; 
+	}
+};
+ 
+void addEdge(vector<int> adj[],int u,int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+int main()
+{
+    vector<int> adj[6];
+    
+    addEdge(adj,1,2);
+    addEdge(adj,1,3);
+    addEdge(adj,1,4);
+    addEdge(adj,1,5);
+    addEdge(adj,2,4);
+    addEdge(adj,2,1);
+    addEdge(adj,3,1);
+    addEdge(adj,4,1);
+    addEdge(adj,4,2);
+    addEdge(adj,5,1);
+    Solution obj;
+    vector<int> df;
+    df= obj.dfsOfGraph(5, adj);
+    for(auto it: df)
+    cout<<it<<" ";
+ 
+    return 0;
+}
+
+/*
+Output:
+
+1 2 4 3 5
+
+Time complexity: O(N+E), Where N is the time taken for visiting N nodes and E is for travelling through adjacent nodes.
+
+Space Complexity:O(N+E)+O(N)+O(N) , Space for adjacency list, Array,Auxiliary space.
+
+*/
+
+// contributed by Sourav Naskar
