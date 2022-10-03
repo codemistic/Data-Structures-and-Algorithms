a9580679154e11ef851f4af24af5255ae67754d7
Kanishka Gour <82566639+kanishka-28@users.noreply.github.com>
2022-10-03 16:19:23 +0530

    Create Leetcode_candy.cpp

---
+++ CPP/Problems/Leetcode_candy.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+     int candy(vector<int>& ratings) {
+      int n = ratings.size();
+        int candy = n, i=1;
+        while(i<n){
+            if(ratings[i] == ratings[i-1]){
+                i++;
+                continue;
+            }
+            
+            //For increasing slope
+            int peak = 0;
+            while(ratings[i] > ratings [i-1]){
+                peak++;
+                candy += peak;
+                i++;
+                if(i == n) return candy;
+            }
+            
+            //For decreasing slope
+            int valley = 0;
+            while(i<n && ratings[i] < ratings[i-1]){
+                valley++;
+                candy += valley;
+                i++;
+            }
+            candy -= min(peak, valley); //Keep only the higher peak
+        }
+        return candy;   
+    }
+};
