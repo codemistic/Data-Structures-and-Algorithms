d023aa03292ce468db414629eca24f6797c97108
avishagoyal08 <90690345+avishagoyal08@users.noreply.github.com>
2022-10-04 12:55:00 +0530

    Update reverseInteger.cpp

--- Maths/reverseInteger.cpp
+++ Maths/reverseInteger.cpp
@@ -11,12 +11,8 @@ int main()
     while (n != 0)
     {
         int rem = n % 10;
-        n /= 10;
-        if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && rem > 7))
-            return 0;
-        if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && rem < -8))
-            return 0;
         rev = rev * 10 + rem;
+        n /= 10;
     }
     cout << rev << endl;
     return 0;
