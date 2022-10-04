ca92ec02d50e9d602569e873219fc56c42adbd56
ash-sxn <ash.191245141@gmail.com>
2022-10-04 05:34:14 +0530

    Fixed inconsistent class naming of java code (to CamelCase)^C

---
+++ Java/String/CaseConvert.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CaseConvet {
+    
+
+
+
+
+    public static void main(String[] args) {
+       Scanner sc =new Scanner(System.in);
+     
+        String Str =sc.nextLine();
+        System.out.println(Str.toUpperCase());
+    }
+}
