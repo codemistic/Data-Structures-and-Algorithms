e6328b8a16f38b05a1a3b2f8396e9c19b2f3710e
GAURISH OJHA <97218624+GaurishIIITNR@users.noreply.github.com>
2022-10-05 22:44:23 +0530

    Update 240_Search_a_2D_Matrix_II.cpp

--- LeetCode Solutions/240_Search_a_2D_Matrix_II.cpp
+++ LeetCode Solutions/240_Search_a_2D_Matrix_II.cpp
@@ -1,6 +1,8 @@
 // Name : Search_a_2D_Matrix_II
 // Leetcode Problem 240 (Medium)
 // Url : https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+// Approach 1 (Best Approach)
 class Solution
 {
 public:
@@ -20,6 +22,30 @@ public:
         }
         if (matrix[i][j] == target)
             return 1;
+        return 0;
+    }
+};
+
+// Approach 2
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(int i=0;i<matrix[0].size();i++)
+            {
+            if(matrix[0][i]>target)
+                return 0;
+            int low=0,high=matrix.size()-1;
+            while(low<high){
+                int mid=low+(high-low+1)/2;
+                    if(matrix[mid][i]<=target)
+                        low=mid;
+            else
+                high=mid-1;
+            }
+            if(matrix[low][i]==target)
+                return 1;
+        }
         return 0;
     }
 };
