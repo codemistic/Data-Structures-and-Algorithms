dd0d59e91e4654bd2015e55a49848f356ae209f6
Devendra Singh Shekhawat <115137631+DevendraSS@users.noreply.github.com>
2022-10-06 13:26:29 +0530

    Program to bubble sort in an array

---
+++ Bubbe_Sort.c
@@ -0,0 +1,67 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 20
+typedef int itemType;
+
+void fill(itemType L[], int n);
+void show(itemType L[], int n);
+void bubbleSort(itemType L[], int n);
+
+int main()
+{
+    itemType list[MAX];
+    int n;
+
+    printf("Enter the current size of the list: ");
+    scanf("%d", &n);
+
+    fill(list, n);
+    show(list, n);
+    bubbleSort(list, n);
+    show(list, n);
+
+    return 0;
+}
+
+void bubbleSort(itemType L[], int n)
+{
+    int current, i, pass;
+    itemType temp;
+
+    current = n-1;
+    for (pass=0; pass<n-1; ++pass)
+    {
+        for (i=0; i<current; ++i)
+        {
+            if (L[i]>L[i+1])
+            {
+                //swap
+                temp = L[i];
+                L[i] = L[i+1];
+                L[i+1] = temp;
+            }
+            show(L, n);
+            getch();
+        }
+        --current;
+        printf("\n\n\n");
+    }
+}
+
+void fill(itemType L[], int n)
+{
+    int i;
+    srand(time(0));
+    for (i=0; i<n; ++i)
+        L[i] = rand()%100;
+}
+void show(itemType L[], int n)
+{
+    int i;
+    printf("The list is: ");
+    for (i=0; i<n; ++i)
+        printf("%d  ", L[i]);
+    printf("\n\n");
+}
