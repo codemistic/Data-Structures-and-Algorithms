9471e7ca57ecacbcd2b8f35ae9b273a76b649c7b
Yash <yashpatel54257gmail.com>
2022-10-08 13:50:35 +0530

    modified

--- CPP/array-2d/array.cpp
+++ CPP/array-2d/array.cpp
@@ -14,16 +14,29 @@ int main(){
 	// 	cout<<arr[i];
 	// }
 
+int i=0;
+int arr[n][m];
+while (i<n)
+{
+	int j=0;
+	
+	while (j<m)
+	{
+		
+		cin>>arr[i][j];
+		++j;
+	}
+	++i;
+}
 
 	//2D array
-	int arr[n][m];
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			cin>>arr[i][j];
-		}
-	}
+	// for (int i = 0; i < n; ++i)
+	// {
+	// 	for (int j = 0; j < m; ++j)
+	// 	{
+	// 		cin>>arr[i][j];
+	// 	}
+	// }
 	cout<< "Array of "<<n<<" rows and "<<m <<" columns"<<endl;
 	for (int i = 0; i < n; ++i)
 	{
@@ -33,5 +46,7 @@ int main(){
 		}
 		cout<<endl;
 	}
+
+
 	return 0;
 }
