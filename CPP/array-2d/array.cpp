5721e478f7ab7ab47002046ea41ce1b299b32874
Yash <yashpatel54257gmail.com>
2022-10-08 13:15:48 +0530

    array.cpp

--- CPP/array-2d/array.cpp
+++ CPP/array-2d/array.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
 	int n,m;
+	cout<<"Enter first number of rows and then columns"<<endl;
 	cin>>n>>m;
 	// char arr[n];
 	// for (int i = 0; i < n; ++i)
@@ -23,6 +24,7 @@ int main(){
 			cin>>arr[i][j];
 		}
 	}
+	cout<< "Array of "<<n<<" rows and "<<m <<" columns"<<endl;
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j < m; ++j)
