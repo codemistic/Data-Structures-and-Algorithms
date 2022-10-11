da404394ffd98f3d0f06486c65e79da46c018702
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2022-10-11 11:10:07 +0530

    Time complexity remains constant using max.
    Since there are only three number so using max function time complexity remains constant.

--- Python/Largest of the three numbers.py
+++ Python/Largest of the three numbers.py
@@ -7,11 +7,6 @@ num2 = float(input("Enter the second number - "))
 num3 = float(input("Enter the third number - "))
 
 #if, elif, else blocks
-if(num1 >= num2) and (num1 >= num3):
-   largest = num1
-elif(num2 >= num3) and (num2 >= num1):
-   largest = num2
-else: 
-   largest = num3
+largest = max(num1, num2, num3)
 
 print("The largest number is", largest)   
