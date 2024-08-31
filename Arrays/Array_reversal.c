45ace3f0cf5d98712c6ce09e576e92dc0775593c
Jitankasarkar <jitankasarkar2017@gmail.com>
2024-08-31 22:12:34 +0530

    Array reversal

--- Arrays/Array_reversal.c
+++ Arrays/Array_reversal.c
@@ -3,16 +3,19 @@
 
 int main(){
     int arr[N];
-
+    
+    //giving the size of an array
     int n;
     printf("enter the size of array:\n");
     scanf("%d", &n);
 
+    // Printing the array that will be reversed
     printf("Enter the array:\n");
     for(int i=0; i<n; i++){
         scanf("%d", &arr[i]);
     }
-
+    
+    // printing the reversed array
     printf("eversed array:\n");
     for(int i=n-1; i>=0; i--){
         printf("%d ", arr[i]);
